3d7a12fe12edbfc6024535f50b98b376d75085eb
Hilde N <snhilde@protonmail.com>
2020-02-08 18:01:26 -0500

    initial layout commit

---
+++ hsort.go
@@ -0,0 +1,22 @@
+package hsort
+
+import (
+	"errors"
+)
+
+
+func InsertionInt(list []int) error {
+
+	return nil
+}
+
+func SelectionInt(list []int) error {
+
+	return nil
+}
+
+func MergeInt(list []int) error {
+
+	return nil
+}
+
