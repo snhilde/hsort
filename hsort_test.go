fba89641a1d0c49aa897c9315ad2405bcbab0fd2
Hilde N <snhilde@protonmail.com>
2020-02-08 17:59:53 -0500

    prepare test harness

---
+++ hsort_test.go
@@ -0,0 +1,59 @@
+package hsort
+
+import (
+	"testing"
+	"time"
+	"math/rand"
+	"sort"
+)
+
+
+// Build a slice of random numbers and sort it with the provided sorting function.
+// t        testing object
+// sortFunc callback sort function
+// iters    num of iterations to run
+// length   length of slice to sort
+func testSort(t *testing.T, sortFunc func([]int) error, iters int, length int) {
+	for i := 0; i < iters; i++ {
+		seed   := time.Now().UnixNano()
+		source := rand.NewSource(seed)
+		random := rand.New(source)
+
+		// Populate the slice with random values.
+		list := make([]int, length)
+		for i := 0; i < length; i++ {
+			list[i] = random.Int()
+		}
+
+		// Sort the slice using the provided algorithm.
+		listCopy := make([]int, length)
+		copy(listCopy, list)
+		err := sortFunc(listCopy)
+		if err != nil {
+			t.Log("Sorting failed:")
+			t.Error(err)
+		}
+
+		// Check that the sorting algorithm was correct.
+		sort.Ints(list)
+		for i, v := range list {
+			if v != listCopy[i] {
+				t.Error("Values at index", i, "differ")
+				t.Log("list:", v)
+				t.Log("listCopy:", listCopy[i])
+			}
+		}
+	}
+}
+
+func TestInsertionInt(t *testing.T) {
+	testSort(t, InsertionInt, 100, 10000)
+}
+
+func TestSelectionInt(t *testing.T) {
+	testSort(t, SelectionInt, 100, 10000)
+}
+
+func TestMergeInt(t *testing.T) {
+	testSort(t, MergeInt, 100, 10000)
+}
